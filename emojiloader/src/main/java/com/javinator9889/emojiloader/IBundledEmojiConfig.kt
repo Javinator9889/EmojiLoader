b6162e0eb98d34f8752dff37edeee1a1596ee82b
Javinator9889 <dev@javinator9889.com>
2021-04-23 19:42:39 +0200

    docs: documented IBundledEmojiConfig.kt

--- emojiloader/src/main/java/com/javinator9889/emojiloader/IBundledEmojiConfig.kt
+++ emojiloader/src/main/java/com/javinator9889/emojiloader/IBundledEmojiConfig.kt
@@ -21,11 +21,48 @@ package com.javinator9889.emojiloader
 import android.content.Context
 import androidx.emoji.text.EmojiCompat
 
+/**
+ * Global interface that must be implemented by the bundled emoji class
+ * itself. Currently, this is used by [com.javinator9889.bundledemoji.BundledEmojiConfig]
+ * class, which provides an easy-to-use object for loading all the available emojis at once.
+ *
+ * This is interesting for:
+ *  + Loading all emojis directly for slow or old phones.
+ *  + Be able to use every emoji independently if the phone has or not Google Play Services.
+ *  + Use this application with third party providers such as F-Droid or similar.
+ *  + Embed the fonts directly into the device so the app can work mostly offline.
+ *
+ * But, take in mind that embedding the fonts in the APK file carries a HUGE increase in size
+ * (estimated about ~4 MB), so prefer using the Google Play Services font loading.
+ *
+ * @author Javinator9889
+ * @since v1.0
+ */
 interface IBundledEmojiConfig {
     companion object {
+        /**
+         * Package name used for loading the class using reflection. It's immutable and
+         * should not be edited or modified.
+         */
         const val PACKAGE_NAME = "com.javinator9889.bundledemoji"
+
+        /**
+         * Class name used for loading it using reflection. It's immutable and
+         * should not be edited or modified.
+         */
         const val CLASS_NAME = "BundledEmojiConfig"
     }
 
+    /**
+     * By using the provided [context], loads the configuration telling the [EmojiCompat]
+     * library which configuration to use.
+     *
+     * This method should be overridden by any configuration loader that you may use. By default,
+     * the class [EmojiLoader] picks the best option based on your requirements.
+     *
+     * @param[context] the context in which this method is called. Notice that application's context
+     *                 is preferred over the activity's context and/or fragment's context.
+     * @return the specific [EmojiCompat.Config] configuration file used by the project.
+     */
     fun loadConfig(context: Context): EmojiCompat.Config
 }
